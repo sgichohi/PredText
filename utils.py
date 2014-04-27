522404af0268ad9fdfbc6ef3082d83dbf92934df
Nicole Loncke <nloncke15@gmail.com>
2014-04-27 14:16:04 -0400

    added write to file functionality

--- utils.py
+++ utils.py
@@ -1,27 +1,30 @@
-import re
+import json
 
-def email_to_tuple(filenames):
+def email_to_tuple(filenames, to_file=False, output="out"):
     """ Converts a list of the Enron emails to a list of tuples.
     """
     emails = list()
     for mail in filenames:
         f = open(mail, 'r')
         email = dict()
-        for line in f:
-            if line.startswith("From: "):
-                print line.lstrip("From: ").rstrip()
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            elif line.startswith("From: "):
                 email["sender"] = line.lstrip("From: ").rstrip()
             elif line.startswith("To: "):
                 email["recipient"] = line.lstrip("To: ").rstrip()
-                print line.lstrip("To: ").rstrip()
             elif line.startswith("Date: "):
-                email["recipient"] = line.lstrip("Date: ").rstrip()
-                print line.lstrip("Date: ").rstrip()
-            elif line.startswith("X-Filename: "):
+                email["date"] = line.lstrip("Date: ").rstrip()
+            elif line.startswith("X-FileName: "):
                 break
         email["message"] = f.read().strip()
         f.close()
         emails.append(email)
+    if to_file:
+        with open(output, 'w+') as f:
+            f.write(json.dumps(emails))
     return emails
         
             
