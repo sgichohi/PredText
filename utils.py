cba56ca80742dcd3715a88940e6d89ab6ff94eda
Kiiru Gichohi <samuel.kiiru@gmail.com>
2014-05-01 21:35:33 -0400

    db funcs

--- utils.py
+++ utils.py
@@ -1,5 +1,9 @@
-import json, os
+import json
+import os
 import commands as cmd
+import sqlite3 as sq
+import sys
+
 
 def email_to_tuple(filenames, to_file=False, output="out"):
     """ Converts a list of the Enron emails to a list of tuples.
@@ -31,6 +35,8 @@ def email_to_tuple(filenames, to_file=False, output="out"):
     return emails
 
 # gather all enron email files under specified location directory
+
+
 def walkdir(location):
     filelist = list()
     for dirname, dirnames, filenames in os.walk(location):
@@ -38,3 +44,31 @@ def walkdir(location):
             if filename.endswith("."):
                 filelist.append(os.path.join(dirname, filename))
     return filelist
+
+
+def set_up():
+    with con:
+        cur.execute(
+            "CREATE TABLE Emails(Sender TEXT, Recipient TEXT, Message TEXT")
+
+
+def insert_email(con, emails):
+
+    with con:
+        cur = con.cursor()
+        cur.executemany("INSERT INTO Emails VALUES(?, ?,?)", emails)
+
+
+def get_emails(sender, recipient):
+    con = sq.connect("test.db")
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT Message from Emails where Recipient=?, Sender=?" recipient, sender)
+        rows = cur.fetchall()
+        return rows
+def
+
+con = sq.connect("test.db")
+emails = ("today", "sam", "cool")
+set_up(con)
+insert_email(con, emails)
