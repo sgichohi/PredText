be3eab72a001ac1c5f1a51ad560b78474dae080b
Nicole Loncke <nloncke15@gmail.com>
2014-05-09 16:17:13 -0400

    beginning to handle forwarded emails

--- utils.py
+++ utils.py
@@ -1,19 +1,32 @@
 import json, os, re, string
 import commands as cmd
-import sqlite3 as sq
+# import sqlite3 as sq
 import sys
 
 
 def email_to_tuple(filenames, to_file=False, output="out"):
     """ Converts a list of the Enron emails to a list of tuples.
+    Input:
+    ------
+    List of filenames with absolute paths.
+
+    Output:
+    -------
+    Tuple representation of an email.
+    (sender, recipient, message)
     """
+
     emails = list()
     for mail in filenames:
+        is_forwarded = False
         f = open(mail, 'r')
         email = dict()
         while True:
             line = f.readline()
             if not line:
+                break
+            elif line.rfind("-Original Message-") is not -1:
+                is_forwarded = True
                 break
             elif line.startswith("From: "):
                 email["sender"] = line.lstrip("From: ").rstrip()
@@ -23,10 +36,13 @@ def email_to_tuple(filenames, to_file=False, output="out"):
                 email["date"] = line.lstrip("Date: ").rstrip()
             elif line.startswith("X-FileName: "):
                 break
-        body = f.read().strip()
+        if is_forwarded:
+            body = "".join([line, f.read().strip()])
+        else:
+            body = f.read().strip()
         f.close()
         email["message"] = body
-        # email["message"] = parse_message(body)
+        # email["message"] = parse_message(body, is_forwarded)
         emails.append( tuple([email["sender"], email["recipient"], email["message"]]) )
     return tuple(emails)
 
@@ -43,14 +59,17 @@ def walkdir(location):
     return filelist
 
 
-def parse_message(body):
-    """ Given the body of an email as a string, returns a comma-separated
-    string of words, all lowercase.  Words deemed unimportant include: one-letter
-    words, numbers, punctuation...
+def parse_message(body, is_forwarded):
+    """ Given the body of an email as a string, returns a
+    comma-separated string of ALL words, all lowercase.  If
+    message is forwarded, ignores forwarded text.
     """
+    if is_forwarded:
+
+
     tokens = re.findall(r"[\w']+" + string.punctuation, body)
 
-    # handle forwarded emails
+    return
 
 
 
